ae89e7e4b77ff8504ef6a8337ad685c9493f0f61
Øystein Krog <oystein.krog@gmail.com>
2014-06-19 10:35:42 +0200

    Fix path in compile.sh so makefile can find teensy flash utility

--- keyboard/ergodox/compile.sh
+++ keyboard/ergodox/compile.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
-export PATH="$PATH:/cygdrive/c/WinAVR-20100110/utils/bin:/cygdrive/c/WinAVR-20100110/bin"
+export PATH="$PATH:/cygdrive/c/WinAVR-20100110/utils/bin:/cygdrive/c/WinAVR-20100110/bin:`pwd`"
 
 make -f Makefile.lufa krog teensy
