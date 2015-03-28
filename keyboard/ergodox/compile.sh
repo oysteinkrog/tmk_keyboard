f4aeb478f41c852699510ac9387eb16e256eff6f
Øystein Krog <oystein.krog@gmail.com>
2015-03-28 17:07:00 +0100

    fix compile.sh

--- keyboard/ergodox/compile.sh
+++ keyboard/ergodox/compile.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
-export PATH="$PATH:/cygdrive/c/WinAVR-20100110/utils/bin:/cygdrive/c/WinAVR-20100110/bin:`pwd`"
+export PATH="$PATH:/d/ERGODOX/WinAVR-20100110/utils/bin:/d/ERGODOX/WinAVR-20100110/bin:`pwd`"
 
 make -f Makefile.lufa krog teensy
